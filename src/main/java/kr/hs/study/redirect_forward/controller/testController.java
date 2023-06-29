130dc17d481001b89c3f2d7b3dcec3e0d334fe2f
Juwan Lee <ljw05w5@gmail.com>
2023-06-29 14:41:55 +0900

    redirect와 forward

--- src/main/java/kr/hs/study/redirect_forward/controller/testController.java
+++ src/main/java/kr/hs/study/redirect_forward/controller/testController.java
@@ -6,4 +6,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class testController {
 
+    @GetMapping("/redirect")
+    public String redirect(){
+        return "redirect:result";
+    }
+
+    @GetMapping("/result")
+    public String result(){
+        return "result";
+    }
+
+    @GetMapping("/forward")
+    public String forward(){
+        return "forward:result";
+    }
 }
